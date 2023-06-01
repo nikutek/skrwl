43ccfea72a5275e544b57b791c5f5ebe11b29794
Kotejichizu <n.golawski@wp.pl>
2023-06-01 18:45:17 +0200

    Dodanie blank stron History i Options

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
+import HistoryPage from "./pages/HistoryPage";
+import OptionsPage from "./pages/OptionsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    children: [{ path: "", element: <HomePage /> }],
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "/history", element: <HistoryPage /> },
+      { path: "/options", element: <OptionsPage /> },
+    ],
   },
 ]);
 
---
+++ src/pages/OptionsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OptionsPage = () => {
+  return <h1>Options Page</h1>;
+};
+
+export default OptionsPage;
