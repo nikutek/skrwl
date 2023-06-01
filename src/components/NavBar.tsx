6dca94f1fd4663811b00e41af546fe84490f732f
Kotejichizu <n.golawski@wp.pl>
2023-06-01 18:45:17 +0200

    Dodanie blank stron History i Options

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavButton from "../components/NavButton";
 
-import { faHouse, faHistory } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faHistory, faGears } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
-    <nav className=" h-full flex flex-col justify-center items-center gap-7 py-3 w-1/12 bg-gray-800 text-center">
+    <nav className=" h-full flex flex-col justify-center items-center gap-7 py-3 w-1/12 bg-gray-800 text-center shadow-xl shadow-gray-600">
       <NavButton destination="/" icon={faHouse}></NavButton>
       <NavButton destination="/history" icon={faHistory}></NavButton>
+      <NavButton destination="/options" icon={faGears}></NavButton>
     </nav>
   );
 };
---
+++ src/pages/HistoryPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HistoryPage = () => {
+  return <h1>History Page</h1>;
+};
+
+export default HistoryPage;
