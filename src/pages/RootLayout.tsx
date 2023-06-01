a4636984e818cc03acd3705dfa815505f8f8a7ff
Kotejichizu <n.golawski@wp.pl>
2023-06-01 18:00:28 +0200

    Navbar i male poprawki

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import NavButton from "../components/NavButton";
+
+import { faHouse, faHistory } from "@fortawesome/free-solid-svg-icons";
+
 const RootLayout = () => {
   return (
-    <div className=" bg-gray-950 h-screen w-screen text-gray-200">
+    <div className=" bg-gray-950 h-screen w-screen flex flex-row">
+      <nav className=" h-full flex flex-col justify-center items-center gap-7 py-3 w-1/12 bg-gray-800 text-center">
+        <NavButton destination="/" icon={faHouse}></NavButton>
+        <NavButton destination="/history" icon={faHistory}></NavButton>
+      </nav>
+
       <Outlet />
     </div>
   );
