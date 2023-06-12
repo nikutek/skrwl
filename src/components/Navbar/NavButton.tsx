8b8c0ade649edba712ec950fbfe43df077bb805e
Kotejichizu <n.golawski@wp.pl>
2023-06-12 10:18:58 +0200

    drobne zmiany w nawigacji

--- src/components/Navbar/NavButton.tsx
+++ src/components/Navbar/NavButton.tsx
@@ -10,10 +10,10 @@ interface props {
 
 const NavButton: FC<props> = ({ destination, icon }) => {
   const activeStyles =
-    "flex justify-center items-center p-2 bg-sky-500 w-12 h-12 rounded-full duration-150 ease-in-out";
+    "flex justify-center items-center p-2 bg-sky-500 w-12 h-12 rounded-full duration-150 ease-in-out md:w-3/4";
 
   const nonActiveStyles =
-    "flex justify-center items-center border-sky-500 border-4 p-2 bg-gray-700 w-12 h-12 rounded-full duration-150 ease-in-out hover:bg-sky-700 hover:text-2xl hover:border-none hover:saturate-50";
+    "flex justify-center items-center border-sky-500 border-4 p-2 bg-gray-700 w-12 h-12 rounded-full duration-150 ease-in-out hover:bg-sky-700 hover:text-2xl hover:border-none hover:saturate-50 md:w-3/4";
 
   return (
     <NavLink
