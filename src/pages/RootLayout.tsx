c55be8cdb7f1c25954c5026bab68b14d21d2b258
Kotejichizu <n.golawski@wp.pl>
2023-06-13 12:11:02 +0200

    poprawki layoutu i troche training page ruszony

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -5,9 +5,9 @@ import NavBar from "../components/Navbar/NavBar";
 
 const RootLayout = () => {
   return (
-    <div className=" flex min-h-screen w-screen  flex-col-reverse overflow-x-hidden bg-gray-950 md:flex-row">
+    <div className="overflow-hidden bg-gray-950 md:flex-row">
       <NavBar />
-      <div className="mb-[calc(100vh/10)] grow md:mb-0 md:ml-[calc(100vw/8)] md:w-[calc(100vw-(100vw/8))]">
+      <div className="fixed right-0 h-[calc(100vh*0.9)] w-full md:h-full md:w-[calc(100vw-(100vw/10))]">
         <Outlet />
       </div>
     </div>
