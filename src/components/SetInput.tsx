d2e5dceea6a0deb92b48c3b692f1530d9e06bea1
Kotejichizu <n.golawski@wp.pl>
2023-06-14 19:33:06 +0200

    training page prawie dzialajacy

---
+++ src/components/SetInput.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+function RepInput({ submitSet, set }) {
+  const [reps, setReps] = useState(10);
+  const [weight, setWeight] = useState(8);
+
+  const submitButtonHandler = () => {
+    console.log("submit");
+    submitSet(reps, weight);
+  };
+
+  const incrementRepBtnHandler = () => {
+    setReps((currentValue) => {
+      return currentValue + 0.5;
+    });
+  };
+  const decrementRepBtnHandler = () => {
+    setReps((currentValue) => {
+      return currentValue > 0 ? currentValue - 0.5 : 0;
+    });
+  };
+
+  const incrementWeightBtnHandler = () => {
+    setWeight((currentValue) => {
+      return currentValue + 0.5;
+    });
+  };
+  const decrementWeightBtnHandler = () => {
+    setWeight((currentValue) => {
+      return currentValue > 0 ? currentValue - 0.5 : 0;
+    });
+  };
+
+  return (
+    <li className="flex w-full flex-col items-center">
+      <h2 className="w-fit whitespace-nowrap text-2xl font-thin">SET {set}</h2>
+      <div className="grow text-center">
+        <div className="mx-2 flex items-center">
+          <label className=" grow text-xl">Repetitions:</label>
+          <input
+            type="number"
+            className="m-4 mx-2 w-1/4  rounded-sm border-2 border-sky-500 bg-gray-500 p-1 text-center text-xl"
+            value={reps}
+            onChange={(e) => {
+              setReps(parseInt(e.target.value));
+            }}
+            required
+          />
+
+          <button
+            className=" mx-1 w-10 rounded-xl border-2  bg-sky-500 py-1 text-lg font-extrabold"
+            onClick={incrementRepBtnHandler}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+
+          <button
+            className="mx-1 w-10 rounded-xl border-2 bg-sky-500 py-1 text-lg font-extrabold"
+            onClick={decrementRepBtnHandler}
+          >
+            <FontAwesomeIcon icon={faMinus} />
+          </button>
+        </div>
+
+        <div className="mx-2 flex items-center">
+          <label className="grow text-xl ">Weight:</label>
+          <input
+            type="number"
+            className="m-4 mx-2 w-1/4 rounded-sm border-2 border-sky-500 bg-gray-500 p-1 text-center text-xl"
+            value={weight}
+            onChange={(e) => {
+              setWeight(parseInt(e.target.value));
+            }}
+            required
+          />
+
+          <button
+            className=" mx-1 w-10 rounded-xl border-2  bg-sky-500 py-1 text-lg font-extrabold"
+            onClick={incrementWeightBtnHandler}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+
+          <button
+            className="mx-1 w-10 rounded-xl border-2 bg-sky-500 py-1 text-lg font-extrabold"
+            onClick={decrementWeightBtnHandler}
+          >
+            <FontAwesomeIcon icon={faMinus} />
+          </button>
+        </div>
+
+        <button
+          className=" w-2/3 rounded-xl border-2 bg-sky-500 py-1 text-2xl"
+          onClick={submitButtonHandler}
+        >
+          Submit
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default RepInput;
--- src/pages/TrainingPage.tsx
+++ src/pages/TrainingPage.tsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectTraining } from "../storage/trainingSlice";
-import RepInput from "../components/RepInput";
+import RepInput from "../components/SetInput";
+
+interface setInfo {
+  reps: number;
+  weight: number;
+}
 
 function TrainingPage() {
+  const [set, setSet] = useState(1);
+  const [doneSets, setDoneSets] = useState<setInfo[]>([]);
+  const [score, setScore] = useState(0);
   const training = useSelector(selectTraining);
-  console.log(training);
+
+  useEffect(() => {
+    if (set === 3) {
+      console.log("koniec");
+      // zapisanie wynikow if zalogowany
+    }
+
+    console.log(doneSets, set, score);
+  }, [set]);
+
+  const submitSet = (reps, weight) => {
+    setSet((currentValue) => {
+      return currentValue + 1;
+    });
+    setScore((currentValue) => {
+      return currentValue + reps * weight;
+    });
+
+    setDoneSets((currentValue) => {
+      // console.log([...currentValue, { reps: reps, weight: weight }]);
+      return [...currentValue, { reps: reps, weight: weight }];
+    });
+  };
 
   if (training.length > 0) {
     return (
-      <div className="col flex h-full  flex-col items-center justify-around  overflow-hidden ">
-        <h1 className=" text-4xl font-bold text-sky-500">{training[0].name}</h1>
+      <div className="col flex h-fit  flex-col items-center justify-center  overflow-scroll ">
+        <h1 className=" m-10 text-4xl font-bold text-sky-500">
+          {training[0].name} {set}
+        </h1>
 
-        <ul className=" flex h-1/2 w-full flex-col items-center justify-around">
-          <RepInput />
-          <li>Seria 2</li>
-          <li>Seria 3</li>
+        <ul className=" flex w-full flex-col items-center justify-around ">
+          {doneSets.map((set, idx) => {
+            return (
+              <li className="flex w-full flex-row p-4 brightness-50">
+                <p className="grow text-xl ">SET {idx + 1}</p>
+                <div>
+                  {set.reps} REPS / {set.weight}kg
+                </div>
+              </li>
+            );
+          })}
+          <RepInput submitSet={submitSet} set={set} />
         </ul>
-
-        <button className="  h-1/12 w-1/3 rounded-lg border-4 border-sky-500 bg-gray-600 p-4 text-2xl transition-all hover:bg-sky-500 hover:font-bold hover:tracking-widest">
-          Done
-        </button>
+        {doneSets.length >= 3 && (
+          <button className="m-4 w-1/2 rounded-xl border-2 bg-sky-500 py-1 text-2xl">
+            Training Done
+          </button>
+        )}
       </div>
     );
   }
