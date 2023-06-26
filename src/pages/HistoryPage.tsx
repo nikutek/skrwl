c3ea498f7d9b63fb9f9c0f5db9711414acadd752
Kotejichizu <n.golawski@wp.pl>
2023-06-26 19:41:46 +0200

    fetchowanie danych do historii treningow

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUserToken } from "../storage/userSlice";
+import { useNavigate } from "react-router-dom";
+
+interface training {
+  date: string;
+  exercise: string;
+  score: number;
+}
 
 const HistoryPage = () => {
-  return <h1>History Page</h1>;
+  const [trainings, setTrainings] = useState<training[]>([]);
+  const userToken = useSelector(selectUserToken);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userToken) {
+      navigate("/");
+    }
+
+    const GetData = async () => {
+      const response = await fetch(
+        `https://skurwiel-auth-default-rtdb.europe-west1.firebasedatabase.app/trainings/${userToken}.json`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+      const jsonData = await response.json();
+
+      console.log(Object.entries(jsonData));
+      setTrainings(jsonData);
+    };
+    GetData();
+  }, [userToken]);
+
+  return (
+    <div>
+      <h1>History Page</h1>
+      {Object.entries(trainings).map((training) => {
+        return <p>{training[1].exercise}</p>;
+      })}
+    </div>
+  );
 };
 
 export default HistoryPage;
