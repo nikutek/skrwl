137c2df6f12b9315ab6701f958b5ec54fdb477b4
Kotejichizu <n.golawski@wp.pl>
2023-06-28 17:38:47 +0200

    graphJs2 wynikow cwiczen na statycznych danych

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,18 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUserToken } from "../storage/userSlice";
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
-interface training {
-  date: string;
-  exercise: string;
-  score: number;
-}
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const HistoryPage = () => {
-  const [trainings, setTrainings] = useState<training[]>([]);
+  const [trainings, setTrainings] = useState({});
   const userToken = useSelector(selectUserToken);
   const navigate = useNavigate();
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
 
   useEffect(() => {
     if (!userToken) {
@@ -33,17 +61,76 @@ const HistoryPage = () => {
       const jsonData = await response.json();
 
       console.log(Object.entries(jsonData));
-      setTrainings(jsonData);
+
+      // Preparowanie danych do wykresu
+
+      const data = [
+        {
+          labels: ["26.06.2023", "28.06.2023"],
+          datasets: [{ labels: "Score", data: [240, 400], tension: 0.4 }],
+        },
+      ];
+
+      // Object.entries(jsonData).forEach((ExerciseData) => {
+      //   if (labels.includes(ExerciseData[1].date)) {
+      //     console.log("juz jest");
+      //   } else {
+      //     labels.push(ExerciseData[1].date);
+      //     datasets.push({ label: "Score", data: 0 });
+      //   }
+      //   // znalezienie indeksu
+
+      //   datasets[labels.indexOf(ExerciseData[1].date)].data +=
+      //     ExerciseData[1].score;
+      // });
+
+      //   console.log({ labels, datasets });
+
+      //   setTrainings({
+      //     labels: labels,
+      //     datasets: { labels: "Score", data: [24, 48] },
+      //   });
+      //   console.log(trainings);
     };
+
     GetData();
   }, [userToken]);
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [1, 2, 3, 4, 5, 6, 7],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: [1, 2, 3, 4, 5, 6, 7],
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
 
   return (
     <div>
       <h1>History Page</h1>
-      {Object.entries(trainings).map((training) => {
+      <Line options={options} data={data} />
+      {/* {Object.entries(trainings).map((training) => {
         return <p>{training[1].exercise}</p>;
-      })}
+      })} */}
     </div>
   );
 };
