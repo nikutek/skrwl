4be55d9f56f31454d818a09d5b4c6422c914485d
Kotejichizu <n.golawski@wp.pl>
2023-06-29 14:54:09 +0200

    tabela wynikow treningow

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -12,7 +12,13 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import type { ChartData, ChartOptions } from "chart.js";
 import { Line } from "react-chartjs-2";
+
+interface LineProps {
+  options: ChartOptions<"line">;
+  data: ChartData<"line">;
+}
 
 ChartJS.register(
   CategoryScale,
@@ -28,19 +34,8 @@ const HistoryPage = () => {
   const [trainings, setTrainings] = useState({});
   const userToken = useSelector(selectUserToken);
   const navigate = useNavigate();
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top" as const,
-      },
-      title: {
-        display: true,
-        text: "Chart.js Line Chart",
-      },
-    },
-  };
+  const [chartData, setChartData] = useState<ChartData>();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!userToken) {
@@ -59,78 +54,114 @@ const HistoryPage = () => {
         }
       );
       const jsonData = await response.json();
-
-      console.log(Object.entries(jsonData));
-
-      // Preparowanie danych do wykresu
-
-      const data = [
-        {
-          labels: ["26.06.2023", "28.06.2023"],
-          datasets: [{ labels: "Score", data: [240, 400], tension: 0.4 }],
-        },
-      ];
-
-      // Object.entries(jsonData).forEach((ExerciseData) => {
-      //   if (labels.includes(ExerciseData[1].date)) {
-      //     console.log("juz jest");
-      //   } else {
-      //     labels.push(ExerciseData[1].date);
-      //     datasets.push({ label: "Score", data: 0 });
-      //   }
-      //   // znalezienie indeksu
-
-      //   datasets[labels.indexOf(ExerciseData[1].date)].data +=
-      //     ExerciseData[1].score;
-      // });
-
-      //   console.log({ labels, datasets });
-
-      //   setTrainings({
-      //     labels: labels,
-      //     datasets: { labels: "Score", data: [24, 48] },
-      //   });
-      //   console.log(trainings);
+      return jsonData;
     };
 
-    GetData();
+    GetData().then((datapoints) => {
+      console.log(datapoints);
+      let dates: string[] = Object.entries(datapoints).map((ExerciseData) => {
+        return ExerciseData[1].date;
+      });
+      dates = [...new Set(dates)];
+      console.log(dates);
+
+      const scores: number[] = dates.map((date) => {
+        let scoreSum = Object.entries(datapoints)
+          .filter((entry) => {
+            return entry[1].date === date;
+          })
+          .map((entry) => {
+            return entry[1].score;
+          });
+
+        return scoreSum.reduce((partialSum, a) => partialSum + a, 0);
+      });
+
+      setChartData({
+        labels: dates,
+        datasets: [
+          {
+            label: "huj",
+            data: scores,
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            tension: 0.4,
+          },
+        ],
+      });
+
+      setIsLoading(false);
+    });
   }, [userToken]);
 
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-  ];
+  // const labels = [
+  //   "January",
+  //   "February",
+  //   "March",
+  //   "April",
+  //   "May",
+  //   "June",
+  //   "July",
+  // ];
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Dataset 1",
-        data: [1, 2, 3, 4, 5, 6, 7],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "Dataset 2",
-        data: [1, 2, 3, 4, 5, 6, 7],
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
+  // const data = {
+  //   labels,
+  //   datasets: [
+  //     {
+  //       label: "Dataset 1",
+  //       data: [1, 2, 10, 4, 5, 6, 7],
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //       tension: 0.4,
+  //     },
+  //   ],
+  // };
+
+  // const options = {
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       position: "top" as const,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: "Chart.js Line Chart",
+  //     },
+  //   },
+  // };
 
   return (
-    <div>
+    <div className="w-full">
       <h1>History Page</h1>
-      <Line options={options} data={data} />
-      {/* {Object.entries(trainings).map((training) => {
-        return <p>{training[1].exercise}</p>;
-      })} */}
+      {isLoading && (
+        <h1 className="flex h-full w-full items-center justify-center text-4xl">
+          Loading...
+        </h1>
+      )}
+      {!isLoading && (
+        <div className="flex h-full w-full items-start justify-center p-10">
+          {/* <Line options={options} data={chartData} /> */}
+
+          <table className="w-1/2">
+            <tr>
+              <th className=" py-4 text-2xl font-bold text-sky-500">Date</th>
+              <th className=" py-4 text-2xl font-bold text-sky-500">Score</th>
+            </tr>
+            {chartData?.labels?.map((el, idx) => {
+              return (
+                <tr className=" border-collapse border-2 border-sky-500 font-light">
+                  <th className=" border-collapse border-2 border-sky-500">
+                    {el}
+                  </th>
+                  <th className=" border-collapse border-2 border-sky-500">
+                    {chartData.datasets[0].data[idx]}
+                  </th>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      )}
     </div>
   );
 };
